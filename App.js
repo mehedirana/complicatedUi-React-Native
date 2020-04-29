f6d23e99c4aa0b00437c301f10235034b5e495c7
mehedi rana <mdmehedirana@gmail.com>
2020-04-29 16:59:22 +0600

    add search input and card

--- App.js
+++ App.js
@@ -1,12 +1,65 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, TextInput } from 'react-native';
 import Header from './components/Header';
+import { Card, Title } from 'react-native-paper';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Header/>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header />
+      <ImageBackground style={{ width: "100%", height: "60%" }} source={require('./mycurve.png')} resizeMode='stretch'>
+        <TextInput style={{ borderRadius: 30, backgroundColor: 'white', height: 50, margin: 40, }} />
+      </ImageBackground>
+
+      <View>
+         <View>
+          <Card>
+            <Card.Title/>
+            <Card.Content>
+              <Title>React Native</Title>
+            </Card.Content>
+
+          </Card>
+          <Card>
+            <Card.Title />
+            <Card.Content>
+              <Title>React Native</Title>
+            </Card.Content>
+
+          </Card>
+          <Card>
+            <Card.Title/>
+            <Card.Content>
+              <Title>React Native</Title>
+            </Card.Content>
+
+          </Card>
+        </View>
+         <View>
+          <Card>
+            <Card.Title />
+            <Card.Content>
+              <Title>React Native</Title>
+            </Card.Content>
+
+          </Card>
+          <Card>
+            <Card.Title/>
+            <Card.Content>
+              <Title>React Native</Title>
+            </Card.Content>
+
+          </Card>
+          <Card>
+            <Card.Title/>
+            <Card.Content>
+              <Title>React Native</Title>
+            </Card.Content>
+
+          </Card>
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,6 +67,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f9f9f9',
   },
 });
--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import {Text} from 'react-native';
 import { Appbar } from 'react-native-paper';
+import { Ionicons } from '@expo/vector-icons';
 
 const Header = () =>{
     return (
-      <Appbar.Header>
-        <Appbar.Content
-          title="Title"
-          subtitle="Subtitle"
-        />
+      <Appbar.Header style={{backgroundColor:'white'}}>
+        <Ionicons name="md-menu" size={34} color='#F86262' style={{marginLeft:10}} />
+        <Text style={{marginLeft:20, fontSize:25, fontWeight:'bold',color:'#F86262'}}>Home</Text>
       </Appbar.Header>
     );
  }
