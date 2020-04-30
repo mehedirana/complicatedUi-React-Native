1475a81d56a46b775c93d4ea08e5474247310952
mehedi rana <mdmehedirana@gmail.com>
2020-05-01 03:36:18 +0600

    add view in search screen

--- components/SearchScreen.js
+++ components/SearchScreen.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
 import Header from './Header';
 
-const SearchScreen = ({navigation})=>{
- return(
-     <View style={styles.container}>
-         <Header name="Search"/>
-         <Text>hi i m search View</Text>
-     </View>
- )
+const SearchScreen = ({ navigation }) => {
+    return (
+        <View style={styles.container}>
+            <Header name="Search" />
+            <Card>
+                <Card.Title />
+
+                      <View>
+                           <View></View>
+                           <View></View>
+                           <View></View>
+                      </View>
+                      <View>
+
+                      </View>
+                    <Card.Content>
+                    <Title>React Native</Title>
+                </Card.Content>
+            </Card>
+        </View>
+    )
 }
 
 export default SearchScreen;
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#f9f9f9',
-    //   justifyContent:'center',
-    //   alignItems:'center'
-    
-    },
-  });
+        flex: 1,
+        backgroundColor: '#f9f9f9',
+
+
+    }
+});
