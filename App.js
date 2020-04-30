e20bf0d687c40ff634b27b3bae3e56bca339daae
mehedi rana <mdmehedirana@gmail.com>
2020-04-30 15:10:31 +0600

    add HOME

--- App.js
+++ App.js
@@ -4,16 +4,10 @@ import Header from './components/Header';
 import Search from './components/Search';
 import ViewHolder from './components/ViewHolder';
 
-//const Drawer = createDrawerNavigator();
+const Drawer = createDrawerNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Header />
-      <ImageBackground style={{ width: "100%", height: "60%" }} source={require('./mycurve.png')} resizeMode='stretch'>
-        <TextInput style={{ borderRadius: 30, backgroundColor: 'white', height: 50, margin: 40, }} />
-      </ImageBackground>
-      <ViewHolder/>
-    </View>
+    
   );
 }
 
